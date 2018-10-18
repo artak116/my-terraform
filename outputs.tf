962c2e60372509dbe76e71626cb7c55bc2fc871c
Artak Mehrabyan <artak0211@gmail.com>
2018-10-18 23:37:51 +0400

    ec2 multi-example

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,6 @@
-output "example_public_ip" {
-  value = ["${aws_instance.example.public_ip}", "${aws_instance.example.public_dns}"]
+//output "example_public_ip" {
+////  value = ["${aws_instance.example.public_ip}", "${aws_instance.example.public_dns}"]
+////}
+output "lb_public_ip" {
+  value = "${aws_elb.elb.dns_name}"
 }
--- vars.tf
+++ vars.tf
@@ -7,3 +7,8 @@ variable "ssh_port" {
   description = "SSH Port"
   default = 22
 }
+
+variable "elb_http_port" {
+  description = "HTTP port"
+  default = 80
+}
